5ee84873b75bff590da1ca9a096b5ebf6547e7a0
charlie-casserly <charliecasserly215@gmail.com>
2021-08-18 11:13:09 +0100

    added prices hash in Items initialize

--- lib/items.rb
+++ lib/items.rb
@@ -1,3 +1,36 @@
 class Items
-
+  def initialize
+    prices = [
+      { 
+        item: :pineapple,
+        price: 100,
+        discount: 'first_half_price'
+      },
+      {
+        item: :apple,
+        price: 10,
+        discount: 'two_for_one'
+      },
+      {
+        item: :orange,
+        price: 20,
+        discount: nil
+      },
+      {
+        item: :pear,
+        price: 15,
+        discount: 'two_for_one'
+      },
+      {
+        item: :banana,
+        price: 30,
+        discount: 'half_price'
+      },
+      {
+        item: :mango,
+        price: 200,
+        discount: 'buy_three_get_one_free'
+      }
+    ]
+  end
 end
