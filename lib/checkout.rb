15f602d6b701616242e48798aa2f6a197a83e5b3
charlie-casserly <charliecasserly215@gmail.com>
2021-08-19 12:00:06 +0100

    All base tests for discounts refactored and implemented

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -30,26 +30,7 @@ class Checkout
 
   def cost_counter(item, count)
     database_item = fetch_item(item)
-
-    discount.apply_discount(database_item, count)
-
-    # if item == :apple || item == :pear
-    #   if (count % 2 == 0)
-    #     fetch_price(item) * (count / 2)
-    #   else
-    #     fetch_price(item) * count
-    #   end
-    # elsif item == :banana || item == :pineapple
-    #   if item == :pineapple
-    #     fetch_price(item) * count - (fetch_price(item) / 2)
-    #   else
-    #     (fetch_price(item) / 2) * count
-    #   end
-    # elsif item == :mango
-    #   fetch_price(item) * (count - (count / 4))
-    # else
-    #   fetch_price(item) * count
-    # end
+    database_item[:discount].nil? ? database_item[:price] * count : discount.apply_discount(database_item, count)
   end
 
   def fetch_item(item)
