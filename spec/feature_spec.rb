93e7c7bc56fd95b24e63d943101fd7a797154018
charlie-casserly <charliecasserly215@gmail.com>
2021-08-20 15:39:32 +0100

    Added feature test for a large basket with items that have updated discounts

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -4,7 +4,21 @@ RSpec.describe 'Feature' do
   let(:checkout) { Checkout.new }
 
   context '#total' do 
-    it 'adds up the total cost of a basket where 5 of every fruit has gone through #scan' do 
+    it 'adds up the total cost of a basket where 10 of every fruit has gone through #scan' do 
+      10.times do 
+        checkout.scan('pineapple') 
+        checkout.scan('apple')
+        checkout.scan('orange')
+        checkout.scan('pear')
+        checkout.scan('banana')
+        checkout.scan('mango')
+      end
+
+      expect(checkout.total).to eq(3025)
+    end
+
+    it 'adds up the total cost of a basket where 5 of every fruit has gone through #scan and oranges discount has been updated to half price' do
+      checkout.database.update(item: :orange, discount: :half_price)
       5.times do 
         checkout.scan('pineapple') 
         checkout.scan('apple')
@@ -14,7 +28,7 @@ RSpec.describe 'Feature' do
         checkout.scan('mango')
       end
 
-      expect(checkout.total).to eq(1500)
+      expect(checkout.total).to eq(1450)
     end
   end
 end
